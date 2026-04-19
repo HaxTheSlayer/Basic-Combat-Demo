5e726ab382a47dcecae8c912c078c69a78b7e023
Vallish Rao <vallishironman@gmail.com>
2026-04-19 19:29:07 +0530

    Added Animation Rigging to stop Sword from clipping into the body

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] CharacterController controller;
     [SerializeField] Animator animator;
     [SerializeField] GameInput gameInput;
+    [SerializeField] Rig combatRig;
 
     public float moveSpeed = 2f;
-    
+    [SerializeField] Transform attackPoint; 
+    public float attackRange = 0.5f;
+    [SerializeField] LayerMask enemyLayers; 
+
     private void Update()
     {
         Vector3 camForward = Camera.main.transform.forward.normalized;
@@ -36,9 +41,33 @@ public class Player : MonoBehaviour
             animator.SetFloat("VelX", 0);
             animator.SetFloat("VelZ", 0);
         }
+        if (Input.GetMouseButtonDown(0)) // Left Click
+        {
+            Attack();
+        }
+        Debug.Log("Animator IK Value: " + animator.GetFloat("IK_Weight"));
+        combatRig.weight = animator.GetFloat("IK_Weight");
+
         //if (Input.GetKeyDown(KeyCode.Space))
         //{
 
         //}
+    }
+
+    private void Attack()
+    {
+        // This fires the trigger you just set as a condition
+        animator.SetTrigger("Attack");
+    }
+
+    public void HitTarget()
+    {
+        Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider enemy in hitEnemies)
+        {
+            Debug.Log("Hit: " + enemy.name);
+            // logic for durability loss and enemy damage goes here
+        }
     }
 }
